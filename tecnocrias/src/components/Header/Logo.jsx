68d8bc3591f06b36e534d74a84d5bd2b88e13904
ManuCoutinho <gdn.sasha@gmail.com>
2022-03-12 12:13:55 -0300

    :sparkles: added header component

---
+++ tecnocrias/src/components/Header/Logo.jsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export const Logo = () => {
+  return (
+    <div className={styles.logo}>
+      <h1>Tecnocrias</h1>
+    </div>
+  );
+};
---
+++ tecnocrias/src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import { Logo } from "./Logo";
+import { NavItems } from "./NavItems";
+import styles from "./styles.module.scss";
+
+export const Header = () => {
+  return (
+    <header className={styles.container}>
+      <Logo />
+      <nav className={styles.nav}>
+        <NavItems />
+      </nav>
+    </header>
+  );
+};
