0bac9b69cc5173ff4496f71eb278f33183714859
ManuCoutinho <gdn.sasha@gmail.com>
2022-04-23 11:28:32 -0300

    :recycle: ajustes no diretorio

--- src/components/Desafios/index.jsx
+++ src/components/Desafios/index.jsx
@@ -1,34 +1,17 @@
-import styles from './styles.module.scss'
-import { Card } from './Card'
+import styles from "./styles.module.scss";
+import { Card } from "../../../tecnocrias/src/components/Desafios/Card";
+
 
 export const Desafios = () => {
   return (
     <div className={styles.container}>
       <h2>Desafios</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus numquam
-        commodi omnis deleniti! Reprehenderit fuga quos cupiditate et.
-      </p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Natus numquam commodi omnis deleniti! Reprehenderit fuga quos cupiditate et.</p>
       <div className={styles.cardsContainer}>
-        <div className={styles.grid}>
-          <Card
-            title="100 dias de código"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          ></Card>
-          <Card
-            title="100 dias de código"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          ></Card>
-          <Card
-            title="100 dias de código"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          ></Card>
-          <Card
-            title="100 dias de código"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          ></Card>
-        </div>
+       <Card />
       </div>
+
     </div>
-  )
-}
+  );
+};
