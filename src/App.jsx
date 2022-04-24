f7ec738eb46776564b74cebcdb3946619eb1c129
ManuCoutinho <gdn.sasha@gmail.com>
2022-04-24 11:16:42 -0300

    chore: acertando impotacoes about

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from './components/Header'
 import { Desafios } from './components/Desafios'
+import { About } from './components/About'
 
 function App() {
   return (
     <>
       <Header />
+      <About />
       <Desafios />
     </>
   )
