f689fc4b0e75715cc644998ecfe2ebf32ef46380
ManuCoutinho <gdn.sasha@gmail.com>
2022-04-24 10:44:35 -0300

    chore: accepted merge changes

--- src/components/Desafios/Card.jsx
+++ src/components/Desafios/Card.jsx
@@ -1,19 +1,19 @@
-import styles from "./styles.module.scss";
-import React from "react";
-import CardsItems from "./CardsItems";
+import React from 'react'
+import CardsItems from './CardsItems'
+import styles from './styles.module.scss'
 
 export const Card = () => {
-
-    function getCardsItem() {
-        return CardsItems.map(cards => {
-            return <a href={cards.link}>
-                <div className={styles.card}>
-                    <h3>{cards.title}</h3>
-                    <p>{cards.text}</p>
-                </div>
-            </a>
-        })
-    }
-return (getCardsItem())
+  function getCardsItem() {
+    return CardsItems.map(cards => {
+      return (
+        <a href={cards.link}>
+          <div className={styles.card}>
+            <h3>{cards.title}</h3>
+            <p>{cards.text}</p>
+          </div>
+        </a>
+      )
+    })
+  }
+  return getCardsItem()
 }
-
--- src/components/Desafios/index.jsx
+++ src/components/Desafios/index.jsx
@@ -1,17 +1,17 @@
-import styles from "./styles.module.scss";
-import { Card } from "../../../tecnocrias/src/components/Desafios/Card";
-
+import { Card } from './Card'
+import styles from './styles.module.scss'
 
 export const Desafios = () => {
   return (
     <div className={styles.container}>
       <h2>Desafios</h2>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Natus numquam commodi omnis deleniti! Reprehenderit fuga quos cupiditate et.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus numquam
+        commodi omnis deleniti! Reprehenderit fuga quos cupiditate et.
+      </p>
       <div className={styles.cardsContainer}>
-       <Card />
+        <Card />
       </div>
-
     </div>
-  );
-};
+  )
+}
