b9295959583c833bedcde8cda0be3ce6242c64d7
ManuCoutinho <gdn.sasha@gmail.com>
2023-07-19 20:53:09 -0300

    chore: :rocket: update deploy

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import * as path from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tecnocrias',
   test: {
     globals: true,
     environment: 'jsdom',
